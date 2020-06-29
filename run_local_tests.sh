55a92b4e1b0dcc3e19a6062f20f03b36bd7cd7ad
bigblackbug <bigblackbugg@gmail.com>
2020-06-30 00:44:47 +0300

    feat(hotline): update run_local_tests.sh to accept env file arg

--- run_local_tests.sh
+++ run_local_tests.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-echo 'starting db inside docker'
+[ -z "$1" ] && echo "No such file $1" && exit 1
+
+echo "Using $1"
 set -a
-source 'debug.env'
+source $1
 set +a
 
+echo 'Starting db inside docker'
 postgres_pid=$(docker run -e POSTGRES_PASSWORD="$DB_USER" \
   -e POSTGRES_PASSWORD="$DB_PASS" -p 5432:5432 -d postgres:11)
 echo 'Waiting for db to init'
